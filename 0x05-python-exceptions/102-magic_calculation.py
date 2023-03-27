97deac3ee8e1a6b93325bf42e6a9c0d913bb8eef
Mmulobi <mosesmutush7@gmail.com>
2023-03-27 10:03:53 -0700

    fixed bug

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-
     result = 0
     for i in range(1, 3):
         try:
@@ -12,4 +10,4 @@ def magic_calculation(a, b):
         except Exception:
             result = b + a
             break
-     return result
+    return result

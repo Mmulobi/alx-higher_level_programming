0a28ba92ecfc7fdab80c384102f01576365b96cd
Mmulobi <mosesmutush7@gmail.com>
2023-03-27 08:02:23 -0700

    fix bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    j  = 0
-    for i  in range(x):
+    j = 0
+    for i in range(x):
         try:
             print(my_list[i], end='')
-            j  += 1
+            j += 1
         except Exception as error:
             break
     print('')
